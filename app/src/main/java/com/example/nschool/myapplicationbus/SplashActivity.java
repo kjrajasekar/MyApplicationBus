fc779981fdcf0c58f7a5591512902c68a847f3db
Rajasekar <rajasekar.k@n-school.com>
2018-02-10 17:17:43 +0530

    first commit

--- app/src/main/java/com/example/nschool/myapplicationbus/SplashActivity.java
+++ app/src/main/java/com/example/nschool/myapplicationbus/SplashActivity.java
@@ -1,8 +1,16 @@
 package com.example.nschool.myapplicationbus;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Nschool on 2/10/2018.
  */
 
-class SplashActivity {
+class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
 }
